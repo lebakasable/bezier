fc3db601592273f1212a825e4315f2bc227d9091
LeBakaSable <hugogole.ito@gmail.com>
2024-02-09 17:45:25 +0100

    feat: slider

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <raylib.h>
 #include <stdio.h>
 
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 int main(void) {
   InitWindow(800, 600, "Bézier");
   SetTargetFPS(60);
 
+  float value = 0.5f;
+
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RED);
+    GuiSlider((Rectangle){96, 48, 216, 16}, TextFormat("%0.2f", value), NULL,
+              &value, 0.0f, 1.0f);
     EndDrawing();
   }
 
